1780ae025743c7ee7231ca378e91fff7d2c6cd7a
youtangai <youtanagai@gmail.com>
2018-12-01 16:16:39 +0900

    todolistを実装

---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul>
+        <li v-for="(todo, index) in todoList" v-bind:key="todo.id" @click="done(index)">
+            <s v-if="todo.isDone">{{todo.title}}</s>
+            <span v-else>{{todo.title}}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+
+export default {
+    computed: {
+        ...mapGetters('todo', {
+            todoList:'allTodos',
+        })
+    },
+    methods: {
+        ...mapActions('todo', [
+            'done',
+        ])
+    }
+}
+</script>
