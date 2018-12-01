e78b2d81dbc4ee5c7fa741faacd595b02f2844ab
youtangai <youtanagai@gmail.com>
2018-12-01 16:30:35 +0900

    todo追加フォーム実装

---
+++ src/components/AddTodoForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input type="text" v-model="title" />
+        <input type="button" value="add" @click="add(title)"/>
+    </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+    data:() => {
+        return {
+            title: 'title of todo',
+        }
+    },
+    methods: {
+        ...mapActions('todo', [
+            'add',
+        ])
+    }
+}
+</script>
