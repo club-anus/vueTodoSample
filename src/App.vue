be84135a1219747ed4797b0c0f119382abb573f0
youtangai <youtanagai@gmail.com>
2018-12-02 02:33:47 +0900

    delete logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <AddTodoForm />
     <TodoList />
   </div>
@@ -20,14 +19,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
